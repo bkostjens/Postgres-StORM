6a7b02aaf5e9f13f9f5b313541a00c369641233c
Barry Kostjens <b.kostjens@prosilic.nl>
2019-10-01 09:55:38 +0200

    Throw more explicit error

--- Sources/PostgresStORM/PostgresStORM.swift
+++ Sources/PostgresStORM/PostgresStORM.swift
@@ -98,7 +98,7 @@ open class PostgresStORM: StORM, StORMProtocol {
 		thisConnection.open()
 		if thisConnection.state == .bad {
 			error = .connectionError
-			throw StORMError.error("Connection Error")
+			throw StORMError.connectionError
 		}
 		thisConnection.statement = statement
 
